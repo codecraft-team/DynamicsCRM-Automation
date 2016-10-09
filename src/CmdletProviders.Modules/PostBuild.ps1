a267a92305a58bece9ea823c14eee758dbd486ef
Jozsef Horvath <mail.jozsef.horvath@gmail.com>
2016-10-09 12:44:06 +0200

    IMP: ScriptAnalyzer added to Modules Postbuild again.

--- src/CmdletProviders.Modules/PostBuild.ps1
+++ src/CmdletProviders.Modules/PostBuild.ps1
@@ -9,3 +9,28 @@ Write-Host "`nRemoving Module artifacts..`n";
 @("$TargetDir\$targetName.dll", "$TargetDir\$targetName.pdb") | Remove-Item -Force -Verbose;
 
 Write-Host "`nModule artifacts removed.`n";
+
+If (Get-Module -ListAvailable -Name PSScriptAnalyzer) {
+  $hasError = $false;
+
+  Try {
+    $script = "$($ProjectDir)DynamicsCRM-Automation.ps1";
+    Write-Host "Analyzing script: $($script)";
+    $report = Invoke-ScriptAnalyzer -Severity Error -Path $script;
+    $report | Format-Table;
+    $hasError = $report.Count -gt 0;
+  }
+  Catch {
+    $errorMessage = $_.Exception.Message;
+    Write-Host "Failed to analyze scripts. $($errorMessage)";
+  }
+
+  If ($hasError) {
+    Write-Host "The PSScriptAnalyzer found one or more errors, i.e. quality gate not passed.";
+    $Host.SetShouldExit(1);
+  }
+} 
+Else {
+  Write-Host "Please install PSSCriptAnalyzer in order to verify script quality.";
+  $Host.SetShouldExit(1);
+}
