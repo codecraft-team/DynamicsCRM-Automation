afc9b3ebe9512bd9753c58d53a494ef1b37c8ea6
Jozsef Horvath <mail.jozsef.horvath@gmail.com>
2016-10-09 10:44:44 +0200

    IMP: Still problems with postbuild, trial and error.

--- src/CmdletProviders.Modules/PostBuild.ps1
+++ src/CmdletProviders.Modules/PostBuild.ps1
@@ -4,32 +4,8 @@ param (
   [string]$ProjectDir
 )
 
-Write-Host "`nRemoving Module artifacts..`n"
+Write-Host "`nRemoving Module artifacts..`n";
 
 @("$TargetDir\$targetName.dll", "$TargetDir\$targetName.pdb") | Remove-Item -Force -Verbose;
 
-Write-Host "`nModule artifacts removed.`n"
-
-If (Get-Module -ListAvailable -Name PSScriptAnalyzer) {
-  $hasError = $false;
-
-  Try {
-    $script = "$($ProjectDir)DynamicsCRM-Automation.ps1";
-    Write-Host "Analyzing script: $($script)";
-    #$report = Invoke-ScriptAnalyzer -Severity Error -Path $script;
-    #$report | Format-Table;
-    #$hasError = $report.Count -gt 0;
-  }
-  Catch {
-    Write-Host "Failed to analyze scripts.";
-  }
-
-  If ($hasError) {
-    Write-Host "The PSScriptAnalyzer found one or more errors, i.e. quality gate not passed.";
-    $Host.SetShouldExit(1);
-  }
-} 
-Else {
-  Write-Host "Please install PSSCriptAnalyzer in order to verify script quality.";
-  $Host.SetShouldExit(1);
-}
+Write-Host "`nModule artifacts removed.`n";
